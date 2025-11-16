cc934d69d73f4b5f7d1c4ed54d57b620dbf34382
DadaYaArtem <artem.morozov2004ma@gmail.com>
2025-11-16 18:28:13 +0300

    1

---
+++ problem-solver/cxx/weather-module/weather_module.cpp
@@ -0,0 +1,7 @@
+#include "weather_module.hpp"
+
+#include "agents/update_weather_agent.hpp"
+
+SC_MODULE_REGISTER(WeatherModule)
+->Agent<UpdateWeatherAgent>();
+
---
+++ problem-solver/cxx/weather-module/weather_module.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <sc-memory/sc_module.hpp>
+
+class WeatherModule : public ScModule
+{
+public:
+}
+;
